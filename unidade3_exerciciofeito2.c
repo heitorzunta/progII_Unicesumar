d2d99cc72db16172c6652c0aa128c8d8a023771c
heitorzunta <G2e2C1i0A8n3I>
2020-06-08 22:51:41 -0400

    Unidade 3: Atividade 2 Calcular a quantidade de alunos aprovados e reprovados.

---
+++ unidade3_exerciciofeito2.c
@@ -0,0 +1,48 @@
+/* O problema consiste em auxiliar um professor no fechamento das notas
+    de uma turma. Para tanto, deve ser construído um programa que leia
+    o código do aluno, número de notas e disciplina e as notas. Calcule
+    a média final de cada aluno e informe o númeo de alunos aprovados
+    e reprovados. Para ser aprovado, o estudante precisa obter média
+    maior ou igual a seis. O programa é encerrado quando é informado
+    o código de aluno zero. */
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    int main(){
+
+        int cod, nAprovado = 0, nReprovado = 0, qtsNotas;
+        float nota, media = 0.0, soma = 0.0;
+
+        printf ("Digite a quantidade de notas da matéria: ");
+        scanf  ("%d", &qtsNotas);
+
+        printf ("Digite o codigo do aluno: ");
+        scanf  ("%d", &cod);
+
+        while(cod != 0) {
+
+            for (int i = 0; i < qtsNotas; i++){
+                printf("Digite a nota numero %d: ", i+1);
+                scanf ("%f", &nota);
+                soma +=nota;
+            }
+
+            media = soma / qtsNotas;
+
+            if (media >= 6){
+                nAprovado++;
+            }
+            else {
+                nReprovado++;
+            }
+            printf ("Digite o codigo do aluno: ");
+            scanf  ("%d", &cod);
+
+        }
+
+        printf ("O numero de aprovados é: %d\n", nAprovado);
+        printf ("O numero de reprovados é: %d\n", nReprovado);
+
+        return 0;
+    }
